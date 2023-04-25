59d4020e2ed78b3eb7bb22b47a071042a704a097
Juan Luis Cano Rodríguez <hello@juanlu.space>
2023-04-25 22:43:48 +0200

    Script básico
    - Problemas con logos no cuadrados
    - Problemas con transparencias
    - Coordenadas ligeramente descentradas
    - No valida CSV de entrada

---
+++ autodistintivos.py
@@ -0,0 +1,73 @@
+"""
+Genera distintivos automáticamente.
+"""
+
+__version__ = "0.1.dev0"
+
+
+import csv
+import logging
+from io import BytesIO
+
+import click
+import structlog
+from pypdf import PdfReader, PdfWriter
+from reportlab.pdfgen import canvas
+
+logger = structlog.get_logger()
+
+
+def generate(template, logo, destination):
+    # Adapted from https://stackoverflow.com/a/10766606/554319
+    # Using ReportLab to insert image into PDF
+    logo_bytes = BytesIO()
+    logo_canvas = canvas.Canvas(logo_bytes)
+
+    # Draw image on Canvas and save PDF in buffer
+    logo_canvas.drawImage(logo, x=250, y=500, width=80, height=80)
+    logo_canvas.save()
+
+    # Use PyPDF to merge the image-PDF into the template
+    page = PdfReader(open(template, "rb")).pages[0]
+    overlay = PdfReader(BytesIO(logo_bytes.getvalue())).pages[0]
+    page.merge_page(overlay)
+
+    # Save the result
+    output = PdfWriter()
+    output.add_page(page)
+    with open(destination, "wb") as fh:
+        output.write(fh)
+
+
+def generate_from_data(name, logo_url, template, logos_dir, destination_dir):
+    logo_filename = f"{logos_dir}/{logo_url.rsplit('/', maxsplit=1)[-1]}"
+    destination = f"{destination_dir}/{name}.pdf"
+    try:
+        generate(template, logo_filename, destination)
+    except Exception:
+        logger.error(
+            "No se pudo generar el distintivo", name=name, logo_filename=logo_filename
+        )
+    else:
+        logger.info("Distintivo generado con éxito", name=name)
+
+
+@click.command()
+@click.option("--data-file", required=True)
+@click.option("--template", required=True)
+@click.option("--logos-dir", default="logos")
+@click.option("--destination-dir", default="distintivos")
+def cli(data_file, template, logos_dir, destination_dir):
+    with open(data_file) as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        next(reader)  # CSV header
+        for name, logo_url, *rest in reader:
+            generate_from_data(name, logo_url, template, logos_dir, destination_dir)
+
+
+if __name__ == "__main__":
+    structlog.configure(
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO)
+    )
+
+    cli()
