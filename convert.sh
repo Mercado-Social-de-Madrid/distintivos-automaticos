07a544788c4d0348b38f158d48b230b889c6ce4d
Juan Luis Cano Rodríguez <hello@juanlu.space>
2023-04-26 09:57:12 +0200

    Añade script para convertir PDFs a PNG

---
+++ convert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Thank you, ChatGPT!
+
+# set the directory where your PDF files are located
+PDF_DIR=./distintivos
+
+# loop through all subdirectories of PDF_DIR
+find "$PDF_DIR" -type f -name "*.pdf" -print0 | while IFS= read -r -d $'\0' pdf_file; do
+    # create the PNG file name by replacing the extension of the PDF file with ".png"
+    png_file="${pdf_file%.pdf}.png"
+
+    # convert the PDF file to a PNG file using the convert command from ImageMagick
+    convert -density 300 -quality 100 -colorspace sRGB -profile /usr/share/color/icc/sRGB.icc "$pdf_file" "$png_file"
+done
+
