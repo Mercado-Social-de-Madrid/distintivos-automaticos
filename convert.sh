291bf242bbd4be1e2971eb8b7c9fbafd7bbd0d93
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-04-28 13:59:18 +0200

    Simplifica script de conversión

--- convert.sh
+++ convert.sh
@@ -11,6 +11,5 @@ find "$PDF_DIR" -type f -name "*.pdf" -print0 | while IFS= read -r -d $'\0' pdf_
     png_file="${pdf_file%.pdf}.png"
 
     # convert the PDF file to a PNG file using the convert command from ImageMagick
-    convert -density 300 -quality 100 -colorspace sRGB -profile /usr/share/color/icc/sRGB.icc "$pdf_file" "$png_file"
+    convert -density 300 -quality 100 -colorspace sRGB "$pdf_file" "$png_file"
 done
-
